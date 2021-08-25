1b1b7eaf73c51ed7afb716276f5dd35829e4cc95
Andrey Shtrauss <79809117+shtrausslearning@users.noreply.github.com>
2021-08-25 14:50:58 +0300

    Update cyth_dectree.py

--- Models/cyth_dectree.py
+++ Models/cyth_dectree.py
@@ -1,11 +1,12 @@
-# Cython + Python Implementation of Decision Tree Regression
+# (1) Cython + Python Implementation of Decision Tree Regression
 
-# Evaluation Function
+# (2) Evaluation Function
 
 from sklearn.model_selection import train_test_split as tts
 from sklearn import datasets
 
-def eval_tts(ldf,feature='target', ratio=0.3,model=None):
+# Train/Test Split Evaluation Function
+def eval_tts(ldf,feature='target',subset='train',ratio=0.3,model=None):
     
     # Split feature/target variable
     y = ldf[feature].copy()
@@ -16,15 +17,25 @@ def eval_tts(ldf,feature='target', ratio=0.3,model=None):
     X_train,X_test,y_train,y_test = tts(X,y,test_size=ratio,
                                             random_state=32)
     ym_train = model.fit(X_train,y_train) 
+    if(subset is 'train'):
+        y_model = model.predict(X_train)
+        y_truth = y_train
+        x_dir = [i for i in range(0,y_train.shape[0])]
+    else:
+        y_model = model.predict(X_test)
+        y_truth = y_test
+        x_dir = [i for i in range(0,y_test.shape[0])]
+        
+    ''' Plot Training & Model Data '''
+    model_name = type(model).__name__
     
-    # Plot Training & Model Data (Linear
     fig = go.Figure()
     x_tr = [i for i in range(0,y_train.shape[0])]
-    fig.add_traces(go.Scatter(x=x_tr,y=y_train,mode='lines'))
-    fig.add_traces(go.Scatter(x=x_tr,y=model.predict(X_train),mode='lines'))
+    fig.add_traces(go.Bar(x=x_dir,y=y_truth))
+    fig.add_traces(go.Bar(x=x_dir,y=y_model))
     fig.update_layout(template='plotly_white',
                       margin=dict(l=20, r=20, t=120, b=20),
-                      title='Training | Model & Truth')
+                      title=f'{model_name} | {subset} | Model & Truth')
     fig.update_yaxes(range=[0,50])
     fig.update_layout(showlegend=False,height=300)
     fig.show()
@@ -38,5 +49,10 @@ df_boston = sklearn_to_df(datasets.load_boston())
 y_truth = df_boston['target']
 # display(df_boston.head())
 
+# Train
 model = CDTRegressor(max_depth=10,min_size=10)
 %time y_model = eval_tts(df_boston,feature='target',ratio=0.3,model=model)
+
+# Test
+model = CDTRegressor(max_depth=10,min_size=10)
+%time y_model = eval_tts(df_boston,feature='target',subset='test',ratio=0.3,model=model)
